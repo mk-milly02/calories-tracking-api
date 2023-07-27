1e182689efdfa3eb30d50278a41198599c28583e
Jonas Ababio <jonasagesmilly@outlook.com>
2023-07-27 20:04:16 +0000

    added entities

---
+++ src/calories-api.domain/entities/CalorieEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace calories_api.domain;
+
+public class CalorieEntry
+{
+    public string? Text { get; set; }
+    public double NumberOfCalories { get; set; }
+    public DateTime DateTime { get; set; } = DateTime.Now;
+}
---
+++ src/calories-api.domain/entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace calories_api.domain;
+
+public class User : IdentityUser<Guid>
+{
+    public string? PasswordSalt { get; set; }
+    public double ExpectedNumberOfCaloriesPerDay { get; set; }
+    public bool IsCaloriesDeficient { get; set; }
+    public ICollection<CalorieEntry> Calories { get; set; } = new List<CalorieEntry>();
+}
