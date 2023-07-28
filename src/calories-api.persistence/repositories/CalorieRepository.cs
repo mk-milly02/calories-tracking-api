7b6b24973127c559f69f5d441e4f86f2311a92bc
Jonas Ababio <jonasagesmilly@outlook.com>
2023-07-28 10:30:14 +0000

    implemented calories repository

---
+++ src/calories-api.persistence/repositories/CalorieRepository.cs
@@ -0,0 +1,47 @@
+﻿using calories_api.domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace calories_api.persistence;
+
+public class CalorieRepository : ICalorieRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public CalorieRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<CalorieEntry?> Create(CalorieEntry entry)
+    {
+        EntityEntry<CalorieEntry> added = await _context.Calories.AddAsync(entry);
+        return await _context.SaveChangesAsync() > 0 ? added.Entity : null;
+    }
+
+    public async Task<bool?> Delete(Guid id)
+    {
+        CalorieEntry? existingEntry = await _context.Calories.FindAsync(id);
+
+        if (existingEntry is null) { return null;}
+        _context.Calories.Remove(existingEntry);
+
+        return await _context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<CalorieEntry?> Retrieve(Guid id)
+    {
+        return await _context.Calories.AsNoTracking().SingleOrDefaultAsync(entry => entry.EntryId.Equals(id));
+    }
+
+    public async Task<IEnumerable<CalorieEntry>> RetrieveAll()
+    {
+        return await _context.Calories.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<CalorieEntry?> Update(CalorieEntry entry)
+    {
+        EntityEntry<CalorieEntry> updated = _context.Calories.Update(entry);
+        return await _context.SaveChangesAsync() > 0 ? updated.Entity : null;
+    }
+}
---
+++ src/calories-api.persistence/repositories/interfaces/ICalorieRepository.cs
@@ -0,0 +1,12 @@
+﻿using calories_api.domain;
+
+namespace calories_api.persistence;
+
+public interface ICalorieRepository
+{
+    Task<CalorieEntry?> Create(CalorieEntry entry);
+    Task<CalorieEntry?> Retrieve(Guid id);
+    Task<IEnumerable<CalorieEntry>> RetrieveAll();
+    Task<CalorieEntry?> Update(CalorieEntry entry);
+    Task<bool?> Delete(Guid id);
+}
