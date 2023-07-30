11ebeb75376d7d8c5d39f8a76b3282be002a21f5
Jonas Ababio <jonasagesmilly@outlook.com>
2023-07-30 16:06:14 +0000

    register dependencies

--- src/calories-api.infrastructure/DependencyInjectionExtensions.cs
+++ src/calories-api.infrastructure/DependencyInjectionExtensions.cs
@@ -1,5 +1,14 @@
-﻿using calories_api.services;
+﻿using System.Text;
+using calories_api.domain;
+using calories_api.persistence;
+using calories_api.services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace calories_api.infrastructure;
 
@@ -8,10 +17,47 @@ public static class DependencyInjectionExtensions
     public static void RegisterServices(this IServiceCollection services)
     {
         services.AddHttpClient<IMealService, MealService>();
+
+        services.AddScoped<IMealRepository, MealRepository>();
+        services.AddTransient<IMealService, MealService>();
+        services.AddTransient<IAccountService, AccountService>();
     }
 
     public static void RegisterMappingProfile(this IServiceCollection services)
     {
         services.AddAutoMapper(typeof(MappingProfile));
+    }
+
+    public static void AddTokenBasedAuthentication(this IServiceCollection services, IAuthenticationConfigurationProvider authenticationConfigurationProvider)
+    {
+        IConfiguration bearer = authenticationConfigurationProvider.GetSchemeConfiguration(JwtBearerDefaults.AuthenticationScheme)
+                                ?? throw new InvalidOperationException("Bearer scheme is not configured");
+        
+        services.AddAuthentication(options => 
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options => 
+        {
+            options.TokenValidationParameters = new()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = false,
+                ValidateIssuerSigningKey = true,
+
+                ValidIssuer = bearer.GetValue<string>("issuer"),
+                ValidAudience = bearer.GetValue<string>("audience"),
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(bearer.GetValue<string>("secret-key")!))
+            };
+        });
+    }
+
+    public static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        string? connectionString = configuration.GetConnectionString("calories-api-default");
+
+        services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+        services.AddIdentity<User, Role>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
     }
 }
--- src/calories-api.presentation/Program.cs
+++ src/calories-api.presentation/Program.cs
@@ -1,6 +1,14 @@
+using calories_api.infrastructure;
+using Microsoft.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDatabase(builder.Configuration);
+builder.Services.RegisterMappingProfile();
+builder.Services.RegisterServices();
+builder.Services.AddTokenBasedAuthentication((IAuthenticationConfigurationProvider)builder.Configuration.GetRequiredSection("Bearer"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,29 +24,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
